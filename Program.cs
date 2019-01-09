3e9a43dd605173c102e00145592111af452ac6a9
Tim <timothy.brown@lincoln.oxon.org>
2019-01-09 20:18:00 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace open
+{
+    public class Program
+    {
+        // `open` ShellExecutes its argument.
+        // It parses the CommandLine preserving any quotes (that CLR would otherwise strip out)
+
+        internal static void Main(string[] argsWihtoutQuotes)
+        {
+            var thisProcessNameLen = NextArgLength(Environment.CommandLine);
+            var theCommand = Environment.CommandLine.Substring(thisProcessNameLen);
+
+            var processNameLen = NextArgLength(theCommand);
+            var processName = theCommand.Substring(0, processNameLen).Trim();
+            var processArgs = theCommand.Substring(processNameLen);
+
+            processName = processName.Replace("/", "\\");
+
+            Start(processName, processArgs);
+        }
+
+        // Find the length of the next argument including any surrounding whitespace
+        private static int NextArgLength(string arg)
+        {
+            var len = 0;
+            while (len < arg.Length && char.IsWhiteSpace(arg[len])) len++;
+            var delim = len < arg.Length && arg[len] == '"' ? '"' : ' ';
+            if (delim == '"') len++;
+            while (len < arg.Length && arg[len] != delim) len++;
+            if (len < arg.Length && arg[len] == delim) len++;
+            while (len < arg.Length && char.IsWhiteSpace(arg[len])) len++;
+            return len;
+        }
+
+        // ShellExecute the proc, passing any args. Prints a short error message on failure.
+        private static void Start(string proc, string args)
+        {
+            var psi = new ProcessStartInfo(proc, args) {UseShellExecute = true};
+            try
+            {
+                Process.Start(psi);
+            }
+            catch (Win32Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Open
+
+Opens a file in the appropriate program.
+
+For example, this opens a picture in a preview window:
+
+```
+..\My Pictures> open IMG143994.jpg
+```
+
+The following opens an explorer window in the current directory (Bob's directory):
+
+```
+C:\Users\Bob> open .
+```
+
+This is achieved through shell-execute and file associations set up on your computer. To change the program used to open a particular file, in explorer right-click the file, select "Open With" then "Choose another app". Before choosing the appropriate program, check the "Always use this app" box.
